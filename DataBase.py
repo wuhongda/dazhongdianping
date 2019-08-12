371dc67ca26316aef9c62e786387d8da8a859c8c
wuhongda <49279967+wuhongda@users.noreply.github.com>
2019-08-12 18:12:22 +0800

    重构数据库表不同的标签引用不同的字体库文件

--- DataBase.py
+++ DataBase.py
@@ -26,28 +26,61 @@ if __name__ == '__main__':
     pass
 '''表结构信息
     店铺URL及分类信息表
-    CREATE TABLE `dzcomment_shop` (
-        `id` int(10) NOT NULL AUTO_INCREMENT,
-        `shopid` int(20) NOT NULL COMMENT '店铺对应ID',
-        `shophref` varchar(256) NOT NULL COMMENT '店铺对应的URL',
-        `tag` varchar(40) NOT NULL COMMENT '美食分类',
-        `status` int(4) DEFAULT NULL,
-        PRIMARY KEY (`id`)
-        ) ENGINE=InnoDB AUTO_INCREMENT=49603 DEFAULT CHARSET=utf8
+CREATE TABLE `dzcomment_shop` (
+`id` int(10) NOT NULL AUTO_INCREMENT,
+`shopid` int(20) NOT NULL COMMENT '店铺对应ID',
+`shophref` varchar(256) NOT NULL COMMENT '店铺对应的URL',
+`tag` varchar(40) NOT NULL COMMENT '美食分类',
+`status` int(4) DEFAULT NULL,
+PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB AUTO_INCREMENT=49603 DEFAULT CHARSET=utf8
        
-    商家信息表 
-    CREATE TABLE `shop_detail` (
-        `id` int(10) NOT NULL AUTO_INCREMENT,
-        `shopid` int(20) NOT NULL COMMENT '店铺ID',
-        `name` varchar(256) NOT NULL DEFAULT '-1' COMMENT '店铺名称',
-        `tag` varchar(256) NOT NULL COMMENT '美食类型',
-        `region` varchar(256) NOT NULL COMMENT '行政区',
-        `area` varchar(256) NOT NULL COMMENT '商圈',
-        `avg` varchar(30) NOT NULL COMMENT '人均消费',
-        `flavor` varchar(30) NOT NULL COMMENT '口味',
-        `envir` varchar(30) NOT NULL COMMENT '环境',
-        `service` varchar(30) NOT NULL COMMENT '服务',
-        PRIMARY KEY (`id`),
-        UNIQUE KEY `shopid` (`shopid`)  部分美食属于多种分类 
-        ) ENGINE=InnoDB AUTO_INCREMENT=2632 DEFAULT CHARSET=utf8
+自建字库 word字段为不变信息 每次根据position去匹配
+woff20190806
+Create Table: CREATE TABLE `woff20190806` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(256) NOT NULL DEFAULT '0' COMMENT 'unicode字符名称',
+  `ten_key` varchar(256) NOT NULL DEFAULT '0' COMMENT '十进制字符',
+  `sixteen_key` varchar(256) NOT NULL DEFAULT '0' COMMENT '十六进制字符',
+  `position` text NOT NULL COMMENT 'x，y轴描点坐标',
+  `word` varchar(10) NOT NULL DEFAULT '' COMMENT '对应的解密字体',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1807 DEFAULT CHARSET=utf8
+
+
+用于存放地址信息的woff字体文件
+Create Table: CREATE TABLE `address_dic_tmp` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(256) NOT NULL DEFAULT '0' COMMENT 'unicode字符名称',
+  `ten_key` varchar(256) NOT NULL DEFAULT '0' COMMENT '十进制字符',
+  `sixteen_key` varchar(256) NOT NULL DEFAULT '0' COMMENT '十六进制字符',
+  `position` text NOT NULL COMMENT 'x，y轴描点坐标',
+  `word` varchar(10) NOT NULL DEFAULT '' COMMENT '对应的解密字体',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=603 DEFAULT CHARSET=utf8
+1 row in set (0.00 sec)
+
+用于存放num信息的woff字体文件
+CREATE TABLE `num_dic_tmp` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(256) NOT NULL DEFAULT '0' COMMENT 'unicode字符名称',
+  `ten_key` varchar(256) NOT NULL DEFAULT '0' COMMENT '十进制字符',
+  `sixteen_key` varchar(256) NOT NULL DEFAULT '0' COMMENT '十六进制字符',
+  `position` text NOT NULL COMMENT 'x，y轴描点坐标',
+  `word` varchar(10) NOT NULL DEFAULT '' COMMENT '对应的解密字体',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=588 DEFAULT CHARSET=utf8
+
+
+将两者进行结合每次仅需要在一张表进行匹配数据
+CREATE TABLE `woff_dic` (
+  `id` int(20) NOT NULL,
+  `name` varchar(30) NOT NULL DEFAULT '0' COMMENT 'unicode字符名称',
+  `ten_key` varchar(256) NOT NULL DEFAULT '0' COMMENT '十进制字符',
+  `sixteen_key` varchar(256) NOT NULL DEFAULT '0' COMMENT '十六进制字符',
+  `position` text NOT NULL COMMENT 'x，y轴描点坐标',
+  `word` varchar(10) NOT NULL DEFAULT '' COMMENT '对应的解密字体'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+1 row in set (0.00 sec)
+
 '''
